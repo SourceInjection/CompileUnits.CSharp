23309717af18e35e1e042184ef832282ca3a253d
Florian Reischl <flo.reischl123@gmail.com>
2024-07-02 17:27:07 +0200

    added test project

---
+++ CodeUnits.CSharp.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CodeUnits.CSharp.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
