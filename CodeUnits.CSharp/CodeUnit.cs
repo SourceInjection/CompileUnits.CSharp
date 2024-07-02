f7b55b84bc5e8b99e8f83f2c2f8d4cfaf1ed2a6e
Florian Reischl <flo.reischl123@gmail.com>
2024-07-02 21:46:24 +0200

    Added comments to main entry point

--- CodeUnits.CSharp/CodeUnit.cs
+++ CodeUnits.CSharp/CodeUnit.cs
@@ -8,9 +8,14 @@ using CodeUnits.CSharp.Visitors;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using CodeUnits.CSharp.Exceptions;
 
 namespace CodeUnits.CSharp
 {
+    /// <summary>
+    /// The main entry point for this library.<br/>
+    /// A factory to get a code unit from a <see cref="Stream"/> or a <see langword="string"/>.
+    /// </summary>
     internal class CodeUnit : NamespaceDefinition, ICodeUnit
     {
         private CodeUnit(string projectDefaultNamespace,
@@ -24,33 +29,62 @@ namespace CodeUnits.CSharp
                   externAliases: externAliases)
         { }
 
-        public static ICodeUnit FromFile(string fileName, string projectDefaultNamespace)
+        /// <summary>
+        /// Serializes C# code.
+        /// </summary>
+        /// <param name="stream">The code to parse.</param>
+        /// <param name="projectDefaultNamespace">The default namespace where types and namespaces land if they aren't within a namespace.</param>
+        /// <returns>A <see cref="ICodeUnit"/> which represents serialized C# code.</returns>
+        /// <exception cref="MalformedCodeException"/>
+        /// <exception cref="IOException"/>
+        public static ICodeUnit FromStream(Stream stream, string projectDefaultNamespace = null)
         {
-            return FromStream(new AntlrInputStream(File.OpenRead(fileName)), projectDefaultNamespace);
+            return FromStream(new AntlrInputStream(stream), projectDefaultNamespace);
         }
 
-        public static ICodeUnit FromText(string text, string projectDefaultNamespace)
+        /// <summary>
+        /// Serializes C# code.
+        /// </summary>
+        /// <param name="text">The code to parse.</param>
+        /// <param name="projectDefaultNamespace">The default namespace where types and members land if they aren't within a namespace.</param>
+        /// <returns>A <see cref="ICodeUnit"/> which represents serialized C# code.</returns>
+        /// <exception cref="MalformedCodeException"/>
+        public static ICodeUnit FromString(string text, string projectDefaultNamespace = null)
         {
             return FromStream(new AntlrInputStream(text), projectDefaultNamespace);
         }
 
-        private static ICodeUnit FromStream(ICharStream stream, string projectDefaultNamespace)
+        private static ICodeUnit FromStream(ICharStream stream, string projectDefaultNamespace = null)
         {
-            var lexer = new CSharpLexer(stream);
-            var parser = new CSharpParser(new CommonTokenStream(lexer));
+            if(projectDefaultNamespace == null)
+                projectDefaultNamespace = string.Empty;
 
-            var listener = new ThrowExceptionListener();
-            parser.AddErrorListener(listener);
+            try
+            {
+                var lexer = new CSharpLexer(stream);
+                var parser = new CSharpParser(new CommonTokenStream(lexer));
 
-            var visitor = new NamespaceVisitor();
-            var ns = visitor.VisitCompilation_unit(parser.compilation_unit())
-                ?? throw new InvalidOperationException("something went wrong during parsing");
+                var listener = new ThrowExceptionListener();
+                parser.AddErrorListener(listener);
 
-            return new CodeUnit(projectDefaultNamespace,
-                (IReadOnlyList<UsingDirectiveDefinition>)ns.UsingDirectives,
-                (IReadOnlyList<NamespaceDefinition>)ns.Namespaces, 
-                (IReadOnlyList<TypeDefinition>)ns.Types, 
-                (IReadOnlyList<ExternAliasDefinition>)ns.ExternAliases);
+                var visitor = new NamespaceVisitor();
+                var ns = visitor.VisitCompilation_unit(parser.compilation_unit())
+                    ?? throw new InvalidOperationException("something went wrong during parsing");
+
+                return new CodeUnit(projectDefaultNamespace,
+                    (IReadOnlyList<UsingDirectiveDefinition>)ns.UsingDirectives,
+                    (IReadOnlyList<NamespaceDefinition>)ns.Namespaces,
+                    (IReadOnlyList<TypeDefinition>)ns.Types,
+                    (IReadOnlyList<ExternAliasDefinition>)ns.ExternAliases);
+            }
+            catch (MalformedCodeException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new MalformedCodeException("The imput code is malformed and can not be parsed.", e);
+            }
         }
     }
 }
